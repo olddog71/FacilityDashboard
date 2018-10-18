4d8e786a2a0fb58dc54ebe3ec9c11f8d821d066a
Stacy Smith <stacy.n.claremore@gmail.com>
2018-10-18 05:40:47 -0500

    Add project files.

---
+++ FacilityDashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FacilityDashboard.Startup))]
+namespace FacilityDashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
